fe895228b3a0cc79571f54ecd88ef8384a86ce02
Luiz Passos <lsps1019@gmail.com>
2023-05-19 12:49:39 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+/*
+const barraPesquisa = document.getElementById("search-input");
+const lupaIcon = document.getElementById("lupa-icon");
+
+barraPesquisa.addEventListener("focus", function () {
+  // Aplicar as alterações de estilo nos elementos desejados
+  lupaIcon.style.border = "2px solid #e0e3e5";
+});
+*/
